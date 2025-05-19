10a37acf938efd6e60f45e549aeb285e262f33cb
Sofia <sofiaaitoucheggou@gmail.com>
2025-05-19 16:22:54 +0100

    changes

--- puddle/settings.py
+++ puddle/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -89,10 +90,7 @@ WSGI_APPLICATION = 'puddle.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('postgresql://postgres:egznsYvDTqgjGyOaweCSWTwqmGvTtCNW@crossover.proxy.rlwy.net:49553/railway')
 }
 
 
