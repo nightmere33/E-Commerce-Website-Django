415d6a7f4d8ca43ebf01921d39aa7e5ec133e6d8
MSLLB <wrkmsl02@gmail.com>
2025-05-21 19:24:36 +0100

    video

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,4 +1,11 @@
 from .models import Category
+from django.conf import settings
+
+def media_url(request):
+    return {
+        'MEDIA_URL': settings.MEDIA_URL,
+    }
+
 
 def categories(request):
     return {'categories': Category.objects.all()}
