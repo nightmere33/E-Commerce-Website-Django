04eac217b5e33032f6b8b0a98a91f7e999d1cc1b
NabilChabane27 <chabane.nabil01@gmail.com>
2025-05-27 02:03:24 +0100

    my purchases items empty page design

--- myGames/views.py
+++ myGames/views.py
@@ -1,11 +1,14 @@
-
 from django.shortcuts import render, get_object_or_404, redirect
-from item.models import Item
+from item.models import Item, Category
 from django.contrib.auth.decorators import login_required
 from .models import MyGame
-# Create your views here.
 
 @login_required
 def my_games(request):
-    owned_games =  MyGame.objects.filter(user=request.user).select_related('product')
-    return render(request, 'my_games.html', {'games': owned_games})
+    owned_games = MyGame.objects.filter(user=request.user).select_related('product')
+    # Récupérer toutes les catégories pour la section "Explore by category"
+    categories = Category.objects.all()
+    return render(request, 'my_games.html', {
+        'games': owned_games,
+        'categories': categories
+    })
